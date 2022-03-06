10cd1c399f701eee57f112a62cc6e445c57f124a
OwaisJaved <owaisjaved954@gmail.com>
2022-03-06 14:39:15 +0100

    GraphQl Call for fetching data and exported component

---
+++ src/Components/Action/index.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+const {REACT_APP_GITHUB_ACCESS_TOKEN} = process.env
+//Github GraphQL Query
+export const getGitHubData = async () =>{
+    let fetchedData = await axios.post(
+        "https://api.github.com/graphql",
+        {
+          // Query to Get Required Values
+          //Chaning login name to other username to get that username data
+          query: `{
+                   user(login: "torvalds") {
+                     id  
+                     name
+                     login
+                     avatarUrl
+                     location
+                     login
+                     bio
+                     followers {
+                       totalCount
+                     }
+                     following {
+                       totalCount
+                     }
+                     repositories(first: 50) {
+                       nodes {
+                         id
+                         name
+                         description
+                         stargazerCount
+                         forkCount
+                         createdAt
+                         languages(last: 1) {
+                           nodes {
+                             name
+                           }
+                         }
+                         updatedAt
+                       }
+                       pageInfo {
+                         hasNextPage
+                       }
+                     }
+                   }
+                 }`,
+        },
+        {
+          headers: { Authorization: `Bearer ${REACT_APP_GITHUB_ACCESS_TOKEN}` },
+        }
+      );
+    return fetchedData;
+}
